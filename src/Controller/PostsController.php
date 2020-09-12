63792023f18ccf80cacf19f7343e2b48a9975be9
SIGTTD <sigttd@ya.ru>
2020-09-12 22:51:40 +0300

    add post editing & deleting

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostType;
 use App\Repository\PostRepository;
+use Cocur\Slugify\Slugify;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostsController extends AbstractController
@@ -32,9 +35,75 @@ class PostsController extends AbstractController
     }
 
 	/**
+	 * @Route("/posts/add", name="post_add")
+	 */
+	public function addPost(Request $request, Slugify $slugify)
+	{
+		$post = new Post();
+		$form = $this->createForm(PostType::class, $post);
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$post->setSlug($slugify->slugify($post->getTitle()));
+			$post->setCreatedAt(new \DateTime());
+
+			$em = $this->getDoctrine()->getManager();
+
+			$em->persist($post);
+			$em->flush();
+
+			return $this->redirectToRoute('posts');
+		}
+
+		return $this->render('posts/form.html.twig', [
+			'form' => $form->createView(),
+		]);
+	}
+
+	/**
+	 * @Route("posts/{slug}/edit", name="post_edit")
+	 */
+	public function editPost(Post $post, Request $request, Slugify $slugify)
+	{
+		$form = $this->createForm(PostType::class, $post);
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$post->setSlug($slugify->slugify($post->getTitle()));
+
+			$em = $this->getDoctrine()->getManager();
+
+			$em->flush();
+
+			return $this->redirectToRoute('post_show', [
+				'slug' => $post->getSlug(),
+			]);
+		}
+
+		return $this->render('posts/form.html.twig', [
+			'form' => $form->createView(),
+		]);
+	}
+
+	/**
+	 * @Route("posts/{slug}/delete", name="post_delete")
+	 */
+	public function deletePost(Post $post)
+	{
+		$em = $this->getDoctrine()->getManager();
+
+		$em->remove($post);
+		$em->flush();
+
+		return $this->redirectToRoute('posts');
+	}
+
+	/**
 	 * @Route("/posts/{slug}", name="post_show")
 	 */
-    public function post(Post $post)
+    public function showPost(Post $post)
 	{
 		return $this->render('posts/show.html.twig', [
 			'post' => $post,
